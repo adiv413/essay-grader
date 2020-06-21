2f86c5e4e715b04b13707e4f73a2768fcdf4439f
Aditya Vasantharao <hsfan612@gmail.com>
2020-06-21 12:53:21 -0400

    login testing

--- grader_app/views.py
+++ grader_app/views.py
@@ -114,7 +114,7 @@ def login(request):
                 context['error'] = str(e)
                 args = {"client_id": client_id, "client_secret": client_secret}
                 oauth.refresh_token("https://ion.tjhsst.edu/oauth/token/", **args)
-    if context['error'] == "":
+    if context.has_key('error'):
         context['error'] = "bruh"
     else:
         context['error'] += " sdfjsdflsdkjf"
